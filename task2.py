85010755c74d389ff50cb007b2ca594b36f78083
Lareb Akhmad <c00305960@setu.ie>
2025-09-23 11:01:50 +0000

    lab-2.2

---
+++ task2.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+import re
+def ipdict(line):
+    match = re.search(r'from ([0-9.]+)', line)
+    if match:
+        return match.group(1)
+    return None
+counts = defaultdict(int)
+with open("sample_auth_small.log") as f:
+    for line in f:
+        if "Failed password" in line or "Invalid user" in line:
+            ip = ipdict(line)
+            if ip:
+                counts[ip] += 1
+print(counts)
